5bc9a88613d8c724e54f547de5b78ee4d6e22c60
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-08 13:13:38 +0100

    split into parts and add auth

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::{fs, path::Path};
+use std::{env, fs, path::Path};
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::{Parser, Subcommand};
+use reqwest::header::HeaderMap;
 use scraper::{Html, Selector};
 
 #[derive(Parser)]
@@ -21,15 +22,23 @@ enum Command {
     },
 }
 
-fn parse(html: &str) -> Result<String> {
-    let doc = Html::parse_document(html);
-    let selector = Selector::parse("article.day-desc").expect("slector");
-    let article = doc.select(&selector).next().context("select")?;
-    html2text::from_read(article.inner_html().as_bytes(), 80).context("html2text")
-}
+const USER_AGENT: &str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36";
 
 fn main() -> Result<()> {
     let args = Args::parse();
+    let auth = env::var("AOC_AUTH_COOKIE")
+        .map(|token| format!("session={token}"))
+        .unwrap_or_else(|_| {
+            eprintln!("Must provide auth cookie.");
+            std::process::exit(1)
+        });
+
+    let headers = HeaderMap::from_iter([("cookie".parse()?, auth.parse()?)]);
+
+    let client = reqwest::blocking::Client::builder()
+        .user_agent(USER_AGENT)
+        .default_headers(headers)
+        .build()?;
 
     match args.command {
         Command::Get { year, day } => {
@@ -37,20 +46,22 @@ fn main() -> Result<()> {
                 .join(year.to_string())
                 .join(day.to_string());
 
-            let question_path = dir.join("question.txt");
-
-            let question = if !question_path.exists() {
+            if !dir.exists() {
                 let url = format!("https://adventofcode.com/{year}/day/{day}");
-                let html = reqwest::blocking::get(&url).unwrap().text().unwrap();
-                let text = parse(&html)?;
+                let html = client.get(&url).send().unwrap().text().unwrap();
+                let doc = Html::parse_document(&html);
+                let selector = Selector::parse("article.day-desc").unwrap();
                 fs::create_dir_all(&dir)?;
-                fs::write(&question_path, text.as_bytes())?;
-                text
-            } else {
-                fs::read_to_string(&question_path)?
-            };
-
-            println!("{question}");
+                for (i, article) in doc.select(&selector).enumerate() {
+                    if i > 1 {
+                        eprintln!("found more than 2 articles");
+                        break;
+                    }
+                    let text = html2text::from_read(article.inner_html().as_bytes(), 80)?;
+                    let path = dir.join(format!("part{}q", i + 1));
+                    fs::write(&path, text.as_bytes())?;
+                }
+            }
         }
     }
 
