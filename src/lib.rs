0508193c10b2342c49f1fd8e439523c152ebf78c
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2024-12-09 06:05:23 +0100

    single lib mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-mod client;
-pub use client::{Client, Submit};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
-mod puzzle;
-pub use puzzle::{Puzzle, PuzzleId};
+use anyhow::{Context, Result};
+use reqwest::header::HeaderMap;
+use reqwest::redirect::Policy;
+use scraper::{Html, Selector};
+use tracing::{debug, error, info, warn};
 
 pub const AOC_URL: &str = "https://adventofcode.com";
+pub const AUTH_VAR: &str = "AOC_AUTH_COOKIE";
+pub const CACHE_PATH: &str = "dev/comp/aocli/cache";
+
+pub type PuzzleId = (u32, u32);
 
 #[derive(clap::Parser)]
 #[command(version, author, propagate_version = true)]
 pub struct Args {
+    #[arg(long, short)]
+    pub verbose: bool,
     #[command(subcommand)]
     pub command: Command,
 }
@@ -38,4 +49,221 @@ pub enum Command {
         #[arg(long, short)]
         day: Option<u32>,
     },
+}
+
+fn home_dir() -> PathBuf {
+    PathBuf::from(std::env::var("HOME").unwrap_or_else(|_| {
+        error!("Environment variable`HOME` not found");
+        std::process::exit(1);
+    }))
+}
+
+pub struct Client {
+    http: reqwest::blocking::Client,
+    cache: Cache,
+}
+
+impl Client {
+    pub fn new(token: &str) -> Result<Self> {
+        let mut headers = HeaderMap::new();
+        headers.insert("cookie", format!("sesison={token}").parse()?);
+        Ok(Self {
+            http: reqwest::blocking::Client::builder()
+                .user_agent("aocli.rs")
+                .default_headers(headers)
+                .redirect(Policy::none())
+                .build()?,
+            cache: Cache::new(home_dir().join(CACHE_PATH)),
+        })
+    }
+
+    pub fn get_puzzle(&self, id: &PuzzleId) -> Result<Puzzle> {
+        if let Some(puzzle) = self.cache.get(id) {
+            debug!("puzzle from cache");
+            return Ok(puzzle);
+        }
+        debug!("new puzzle");
+        let puzzle = self.scrape_puzzle(id)?;
+        self.cache.insert(id, &puzzle);
+        Ok(puzzle)
+    }
+
+    fn scrape_puzzle(&self, id: &PuzzleId) -> Result<Puzzle> {
+        let html = self
+            .http
+            .get(self.mkurl(id))
+            .send()
+            .context("get puzzle")?
+            .text()?;
+
+        let doc = Html::parse_document(&html);
+        let query = Selector::parse("article.day-desc").unwrap();
+        let mut questions = doc.select(&query);
+        let q1 = questions
+            .next()
+            .and_then(|el| html2text::from_read(el.inner_html().as_bytes(), 80).ok());
+        let q2 = questions
+            .next()
+            .and_then(|el| html2text::from_read(el.inner_html().as_bytes(), 80).ok());
+
+        let query = Selector::parse("article.day-desc + p code").unwrap();
+        let mut answers = doc.select(&query);
+        let a1 = answers.next().map(|el| el.text().collect::<String>());
+        let a2 = answers.next().map(|el| el.text().collect::<String>());
+
+        Ok(Puzzle {
+            id: id.clone(),
+            q1: q1.unwrap_or_default(),
+            q2: q2.unwrap_or_default(),
+            a1: a1.unwrap_or_default(),
+            a2: a2.unwrap_or_default(),
+        })
+    }
+
+    pub fn get_input(self, id: &PuzzleId) -> Result<String> {
+        if let Some(input) = self.cache.get_input(id) {
+            debug!("input from cache");
+            return Ok(input);
+        }
+        debug!("new input");
+        let input = self
+            .http
+            .get(format!("{}/input", self.mkurl(&id)))
+            .send()
+            .context("get input")?
+            .text()?;
+        self.cache.insert_input(id, &input);
+        Ok(input)
+    }
+
+    pub fn submit(
+        &self,
+        id: &PuzzleId,
+        part: Option<u32>,
+        answer: impl AsRef<str>,
+    ) -> Result<Submit> {
+        let part = part.unwrap_or_else(|| {
+            if fs::metadata(self.cache.mkpath(id).join("answer1")).is_ok_and(|m| m.len() > 0) {
+                2
+            } else {
+                1
+            }
+        });
+
+        let html = self
+            .http
+            .post(format!("{}/answer", self.mkurl(id)))
+            .header("content-type", "application/x-www-form-urlencoded")
+            .body(format!("level={part}&answer={}", answer.as_ref()))
+            .send()?
+            .text()?;
+
+        Ok(if html.contains("That's the right answer") {
+            info!("Correct!");
+            self.refresh_puzzle(id)?;
+            Submit::Correct
+        } else if html.contains("That's not the right answer") {
+            error!("Incorrect!");
+            Submit::Incorrect
+        } else if html.contains("You gave an answer too recently") {
+            warn!("Wait!");
+            Submit::Wait
+        } else {
+            error!("Unknown response");
+            Submit::Error
+        })
+    }
+
+    pub fn refresh_puzzle(&self, id: &PuzzleId) -> Result<Puzzle> {
+        debug!("refresh puzzle");
+        let puzzle = self.scrape_puzzle(id)?;
+        self.cache.insert(id, &puzzle);
+        Ok(puzzle)
+    }
+
+    fn mkurl(&self, (y, d): &PuzzleId) -> String {
+        format!("{AOC_URL}/{y}/day/{d}")
+    }
+}
+
+struct Cache {
+    path: PathBuf,
+}
+
+impl Cache {
+    pub fn new(path: impl AsRef<Path>) -> Self {
+        Self {
+            path: path.as_ref().into(),
+        }
+    }
+
+    pub fn get(&self, id: &PuzzleId) -> Option<Puzzle> {
+        Puzzle::read(self.mkpath(id), id)
+    }
+
+    pub fn get_input(&self, id: &PuzzleId) -> Option<String> {
+        let path = self.mkpath(id).join("input");
+        path.exists()
+            .then(|| fs::read_to_string(self.mkpath(id).join("input")).unwrap())
+    }
+
+    pub fn insert(&self, id: &PuzzleId, puzzle: &Puzzle) {
+        puzzle
+            .write(self.mkpath(&id))
+            .unwrap_or_else(|_| warn!(cause = "failed to insert puzzle", "cache error"));
+    }
+
+    pub fn insert_input(&self, id: &PuzzleId, input: &str) {
+        fs::write(self.mkpath(id).join("input"), input)
+            .unwrap_or_else(|_| warn!(cause = "failed to insert input", "cache error"));
+    }
+
+    #[allow(dead_code)]
+    pub fn update_answer(&self, id: &PuzzleId, part: u32, answer: &str) {
+        fs::write(self.mkpath(id).join(format!("answer{part}")), answer)
+            .unwrap_or_else(|_| warn!(cause = "failed to update answer", "cache error"));
+    }
+
+    fn mkpath(&self, (y, d): &PuzzleId) -> PathBuf {
+        self.path.join(format!("{y}/{d}"))
+    }
+}
+
+pub enum Submit {
+    Correct,
+    Incorrect,
+    Wait,
+    Error,
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Puzzle {
+    pub id: PuzzleId,
+    pub q1: String,
+    pub q2: String,
+    pub a1: String,
+    pub a2: String,
+}
+
+impl Puzzle {
+    pub fn read(path: impl AsRef<Path>, id: &PuzzleId) -> Option<Puzzle> {
+        let path = path.as_ref();
+        path.exists().then(|| Puzzle {
+            id: id.clone(),
+            q1: fs::read_to_string(path.join("question1")).unwrap_or_default(),
+            q2: fs::read_to_string(path.join("question2")).unwrap_or_default(),
+            a1: fs::read_to_string(path.join("answer1")).unwrap_or_default(),
+            a2: fs::read_to_string(path.join("answer2")).unwrap_or_default(),
+        })
+    }
+
+    pub fn write(&self, path: impl AsRef<Path>) -> Result<()> {
+        let path = path.as_ref();
+        fs::create_dir_all(path)?;
+        fs::write(path.join("question1"), self.q1.as_bytes())?;
+        fs::write(path.join("question2"), self.q2.as_bytes())?;
+        fs::write(path.join("answer1"), self.a1.as_bytes())?;
+        fs::write(path.join("answer2"), self.a2.as_bytes())?;
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,34 @@
-use std::{
-    env, fs,
-    path::{Path, PathBuf},
-    process,
-};
+use std::{env, fs, process};
 
 use anyhow::{ensure, Result};
 use clap::Parser;
+use tracing::error;
 
-use aoc::{Args, Client, Command, Puzzle, PuzzleId, Submit};
-use tracing::{debug, error};
-
-fn id_to_path((y, d): (u32, u32)) -> PathBuf {
-    Path::new(&y.to_string()).join(d.to_string())
-}
-
-const AUTH_VAR: &str = "AOC_AUTH_COOKIE";
+use aoc::{Args, Client, Command, PuzzleId, AUTH_VAR};
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    setup_logging()?;
+    setup_logging(args.verbose)?;
 
-    let auth = env::var(AUTH_VAR).unwrap_or_else(|e| {
+    let token = env::var(AUTH_VAR).unwrap_or_else(|e| {
         error!(cause = %e, "Environment variable `{AUTH_VAR}` not found");
         process::exit(1);
     });
 
-    let client = Client::new(&auth)?;
-    let cache = Path::new(&env::var("HOME").unwrap()).join("dev/comp/aocli/cache");
+    let client = Client::new(&token)?;
 
     match args.command {
         Command::Get { year, day, output } => {
             let id = puzzle_id(year, day)?;
-            let puzzle_path = cache.join(id_to_path(id));
-            let puzzle = match Puzzle::read(&puzzle_path, id) {
-                Some(puzzle) => {
-                    debug!("puzzle from cache");
-                    if !puzzle.a1.is_empty() && puzzle.q2.is_empty() {
-                        debug!("next part");
-                        let next_part = client.get_puzzle(&id)?;
-                        next_part.write(&puzzle_path)?;
-                        next_part
-                    } else {
-                        puzzle
-                    }
-                }
-                None => {
-                    let puzzle = client.get_puzzle(&id)?;
-                    puzzle.write(&puzzle_path)?;
-                    puzzle
-                }
-            };
-            let input_path = puzzle_path.join("input");
-            let input = if input_path.exists() {
-                debug!("input from cache");
-                fs::read_to_string(&input_path)?
-            } else {
-                let input = client.get_input(&id)?;
-                fs::write(&input_path, &input)?;
-                input
-            };
-
-            let out = output.unwrap_or("./".into());
-            fs::create_dir_all(&out)?;
+            let puzzle = client.get_puzzle(&id)?;
+            let input = client.get_input(&id)?;
+            let dest = output.unwrap_or("./".into());
+            fs::create_dir_all(&dest)?;
             fs::write(
-                out.join("puzzle.md"),
+                dest.join("puzzle.md"),
                 format!("{}\n{}", puzzle.q1, puzzle.q2),
             )?;
-            fs::write(out.join("input"), &input)?;
+            fs::write(dest.join("input"), &input)?;
         }
 
         Command::Submit {
@@ -76,46 +38,12 @@ fn main() -> Result<()> {
             part,
         } => {
             let id = puzzle_id(year, day)?;
-            let puzzle_path = cache.join(id_to_path(id));
-            let part = part.unwrap_or_else(|| {
-                if fs::metadata(puzzle_path.join("answer1")).is_ok_and(|m| m.len() > 0) {
-                    2
-                } else {
-                    1
-                }
-            });
-            match client.submit(&id, part, &answer)? {
-                Submit::Correct(msg) => {
-                    debug!("{msg}");
-                    if part == 1 {
-                        let puzzle = client.get_puzzle(&id)?;
-                        puzzle.write(&puzzle_path)?;
-                    }
-                    fs::write(puzzle_path.join(format!("answer{part}")), &answer)?;
-                }
-                any => debug!("{any:?}"),
-            }
+            client.submit(&id, part, &answer)?;
         }
 
         Command::View { year, day } => {
             let id = puzzle_id(year, day)?;
-            let puzzle_path = cache.join(id_to_path(id));
-            let puzzle = match Puzzle::read(&puzzle_path, id) {
-                Some(puzzle) => {
-                    if !puzzle.a1.is_empty() && puzzle.q2.is_empty() {
-                        let next_part = client.get_puzzle(&id)?;
-                        next_part.write(&puzzle_path)?;
-                        next_part
-                    } else {
-                        puzzle
-                    }
-                }
-                None => {
-                    let puzzle = client.get_puzzle(&id)?;
-                    puzzle.write(&puzzle_path)?;
-                    puzzle
-                }
-            };
+            let puzzle = client.get_puzzle(&id)?;
             let view = format!("{}\n{}", puzzle.q1, puzzle.q2);
             println!("{view}");
         }
@@ -174,14 +102,19 @@ fn find_current_puzzle_id() -> Option<PuzzleId> {
     None
 }
 
-fn setup_logging() -> Result<()> {
+fn setup_logging(verbose: bool) -> Result<()> {
     use tracing::level_filters::LevelFilter;
     use tracing_subscriber::EnvFilter;
 
     let filter = EnvFilter::builder()
-        .with_default_directive(LevelFilter::DEBUG.into())
+        .with_default_directive(if verbose {
+            LevelFilter::INFO.into()
+        } else {
+            LevelFilter::ERROR.into()
+        })
         .from_env()?
-        .add_directive("hyper::proto=info".parse()?);
+        .add_directive("hyper::proto=info".parse()?)
+        .add_directive("aoc=debug".parse()?);
 
     tracing_subscriber::fmt()
         .with_env_filter(filter)
